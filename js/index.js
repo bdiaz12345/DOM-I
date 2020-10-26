a3185729547f73d0809aed21c12f49d629d1c8d0
bdiaz12345 <bdiazmusic5@gmail.com>
2020-10-26 16:31:33 -0400

    mvp

--- js/index.js
+++ js/index.js
@@ -49,21 +49,27 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 
  let navOne = document.querySelectorAll('a')[0];
  navOne.textContent = siteContent["nav"]["nav-item-1"];
+ navOne.style.color = 'green';
 
  let navTwo = document.querySelectorAll('a')[1];
  navTwo.textContent = siteContent["nav"]["nav-item-2"];
+ navTwo.style.color = 'green';
 
  let navThree = document.querySelectorAll('a')[2];
  navThree.textContent = siteContent["nav"]["nav-item-3"];
+ navThree.style.color = 'green';
 
  let navFour = document.querySelectorAll('a')[3];
  navFour.textContent = siteContent["nav"]["nav-item-4"];
+ navFour.style.color = 'green';
 
  let navFive = document.querySelectorAll('a')[4];
  navFive.textContent = siteContent["nav"]["nav-item-5"];
+ navFive.style.color = 'green';
 
  let navSix = document.querySelectorAll('a')[5];
  navSix.textContent = siteContent["nav"]["nav-item-6"];
+ navSix.style.color = 'green';
 
  let ctaImage = document.querySelector('#cta-img');
  ctaImage.setAttribute('src', 'img/header-img.png');
@@ -71,13 +77,46 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
  let topContent = document.querySelector('.top-content');
  let topText = topContent.querySelectorAll('h4')[0];
  topText.textContent = siteContent["main-content"]["features-h4"]
- topTextTwo = topContent.querySelectorAll('h4')[1];
+ let topTextTwo = topContent.querySelectorAll('h4')[1];
  topTextTwo.textContent = siteContent["main-content"]["about-h4"]
- topParagraph = topContent.querySelectorAll('p')[0];
+ let topParagraph = topContent.querySelectorAll('p')[0];
  topParagraph.textContent = siteContent["main-content"]["features-content"]
- topParagraphTwo = topContent.querySelectorAll('p')[1];
+ let topParagraphTwo = topContent.querySelectorAll('p')[1];
  topParagraphTwo.textContent = siteContent["main-content"]["about-content"]
+
+ let middleImage = document.querySelector('#middle-img');
+ middleImage.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+ let bottomContent = document.querySelector('.bottom-content');
+ let bottomText = bottomContent.querySelectorAll('h4')[0];
+ bottomText.textContent = siteContent["main-content"]["services-h4"]
+ let bottomTextTwo = bottomContent.querySelectorAll('h4')[1];
+ bottomTextTwo.textContent = siteContent["main-content"]["product-h4"]
+ let bottomTextThree = bottomContent.querySelectorAll('h4')[2];
+ bottomTextThree.textContent = siteContent["main-content"]["vision-h4"]
+ let bottomParagraph = bottomContent.querySelectorAll('p')[0];
+ bottomParagraph.textContent = siteContent["main-content"]["services-content"]
+ let bottomParagraphTwo = bottomContent.querySelectorAll('p')[1];
+ bottomParagraphTwo.textContent = siteContent["main-content"]["product-content"]
+ let bottomParagraphThree = bottomContent.querySelectorAll('p')[2];
+ bottomParagraphThree.textContent = siteContent["main-content"]["vision-content"]
+
+ let contactSection = document.querySelector('.contact');
+ let contactMain = contactSection.querySelector('h4');
+ contactMain.textContent = siteContent["contact"]["contact-h4"]
+ let contactParaOne = contactSection.querySelectorAll('p')[0];
+ contactParaOne.textContent = siteContent["contact"]["address"]
+ let contactPhone = contactSection.querySelectorAll('p')[1];
+ contactPhone.textContent = siteContent["contact"]["phone"]
+ let contactEmail = contactSection.querySelectorAll('p')[2];
+ contactEmail.textContent = siteContent["contact"]["email"]
+
+ let footerInfo = document.querySelector('footer');
+ let copyRightInfo = footerInfo.querySelector('p');
+ copyRightInfo.textContent = siteContent["footer"]["copyright"]
+
  
+
 
 
 
